bbc55b4bcde1df55ff71cc5643ced1184216f5e1
realtreeman <zzan9150@gmail.com>
2022-05-25 12:32:54 +0900

    자 바 설 정

---
+++ src/main/java/me/realtree/config/RootConfig.java
@@ -0,0 +1,14 @@
+package me.realtree.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import me.realtree.controller.HomeController;
+
+@Configuration
+public class RootConfig {
+	
+	
+	public HomeController homeController() {
+		return new HomeController();
+	}
+}
